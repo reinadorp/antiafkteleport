d7d2c78a0726658b33950a16bc8b30abdad9515b
ReinadoRP <121303698+reinadorp@users.noreply.github.com>
2023-08-11 22:57:20 -0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.afkTime = 60 -- 1 minute (60 seconds)
+Config.warningTime = 10 -- 10 seconds
+Config.warning = 'You will be teleported in 10 seconds for being AFK.'
+Config.messageteleport = '^1You were transported to a safe zone for being AFK.'
+Config.coords = {x = 217.4830, y = -817.7202, z = 30.5644} -- Put the coordinates where you want to teleport the player when he is AFK
